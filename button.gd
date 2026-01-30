5d6e726b3609bd68e7b4e718dc539dee92272a14
MahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2026-01-30 19:37:03 +0200

    button and mini game

---
+++ button.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+signal pressed(id : int)
+
+var id : int = -1
+
+func _input(event: InputEvent) -> void:
+	var rect := get_global_transform() * get_rect()
+	if(event.is_action_released("mouse") and rect.has_point(get_global_mouse_position())):
+		pressed.emit(id)

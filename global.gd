18a75df0ebaf74f212a6fc1df213494eb3e44dca
MahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2026-02-02 08:34:48 +0200

    score increase

--- global.gd
+++ global.gd
@@ -10,8 +10,31 @@ var easymode := false
 var maxscore = 0
 var maxeasyscore = 0
 
-func save_score():
-	pass
+func save_scores():
+	var scores = {
+		"maxscore" : maxscore,
+		"maxeasyscore" : maxeasyscore
+	}
+	
+	var file = FileAccess.open("user://scores.json",FileAccess.WRITE)
+	
+	var json = JSON.stringify(scores)
+	file.store_string(json)
+	
+
+func _ready() -> void:
+	read_scores()
+
+func read_scores():
+	if(not FileAccess.file_exists("user://scores.json")):
+		return
+	
+	var file := FileAccess.open("user://scores.json",FileAccess.READ)
+	var scoresline := file.get_as_text()
+	var scores = JSON.parse_string(scoresline)
+	
+	maxscore = scores["maxscore"]
+	maxeasyscore = scores["maxeasyscore"]
 
 func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("fullscreen")):
--- losescreen.gd
+++ losescreen.gd
@@ -5,6 +5,8 @@ signal restart
 var score = 0
 var isover = false
 var waseasy = false
+var oldmaxscore := 0
+var oldmaxeasyscore := 0
 
 func enter():
 	modulate.a = 0
@@ -12,6 +14,10 @@ func enter():
 	$CanvasGroup/score.text = "Score: " + str(score)
 	$CanvasGroup/easymodeenbale.modulate = Global.colors["blue"]
 	waseasy = Global.easymode
+	$CanvasGroup/scoredif.hide()
+	$CanvasGroup/scoredif.modulate = Global.colors["green"]
+	oldmaxeasyscore = Global.maxeasyscore
+	oldmaxscore = Global.maxscore
 	if(waseasy):
 		$CanvasGroup/score.modulate = Global.colors["blue"]
 		Global.maxeasyscore = max(Global.maxeasyscore,Global.gamestate.score)
@@ -22,19 +28,26 @@ func enter():
 		$CanvasGroup/easymodeenbale.hide()
 		Global.maxscore = max(Global.maxscore,Global.gamestate.score)
 		disable_easy()
+	Global.save_scores()
 	var tween = create_tween()
 	tween.tween_property(self,"modulate:a",1.,0.3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
 	tween.tween_callback($Timer.start)
 	isover = true
 
-
 func _on_timer_timeout() -> void:
 	if(score < Global.gamestate.score):
 		score += 1
 		$CanvasGroup/score.text = "Score: " + str(score)
+		var curoldmaxscore = 0
+		if(Global.easymode):
+			curoldmaxscore = oldmaxeasyscore
+		else:
+			curoldmaxscore = oldmaxscore
+		if(score > curoldmaxscore):
+			$CanvasGroup/scoredif.show()
+			$CanvasGroup/scoredif.text = "+" + str(score - curoldmaxscore)
 	else:
 		$Timer.stop()
-
 
 func _on_button_pressed(id) -> void:
 	if(not isover):
@@ -43,6 +56,7 @@ func _on_button_pressed(id) -> void:
 	var tween = create_tween()
 	tween.tween_property(self,"modulate:a",0,0.1).set_trans(Tween.TRANS_CIRC)
 	isover = false
+	
 
 func _on_text_button_pressed(id: int) -> void:
 	if(not isover):
@@ -55,10 +69,15 @@ func _on_text_button_pressed(id: int) -> void:
 
 func enable_easy():
 	Global.easymode = true
+	if(waseasy):
+		if(score > oldmaxeasyscore):
+			$CanvasGroup/scoredif.show()
+		else:
+			$CanvasGroup/scoredif.hide()
 	$CanvasGroup/retry.modulate = Global.colors["blue"]
 	$CanvasGroup/easymode.modulate = Global.colors["blue"]
 	$CanvasGroup/maxscore.modulate = Global.colors["blue"]
-	$CanvasGroup/maxscore.text = "Easy Max Score: " + str(Global.maxeasyscore)
+	$CanvasGroup/maxscore.text = "Easy Max Score: " + str(oldmaxeasyscore)
 	var tween = get_tree().create_tween()
 	tween.tween_property($CanvasGroup/easymode,"scale",Vector2(1.1,1.1),0.1)
 	tween.parallel().tween_property($CanvasGroup/easymode,"rotation_degrees",10,0.1)
@@ -66,10 +85,15 @@ func enable_easy():
 
 func disable_easy():
 	Global.easymode = false
+	if(not waseasy):
+		if(score > oldmaxscore):
+			$CanvasGroup/scoredif.show()
+		else:
+			$CanvasGroup/scoredif.hide()
 	$CanvasGroup/retry.modulate = Color.WHITE
 	$CanvasGroup/easymode.modulate = Color.WHITE
 	$CanvasGroup/maxscore.modulate = Color.WHITE
-	$CanvasGroup/maxscore.text = "Max Score: " + str(Global.maxscore)
+	$CanvasGroup/maxscore.text = "Max Score: " + str(oldmaxscore)
 	var tween = get_tree().create_tween()
 	tween.tween_property($CanvasGroup/easymode,"scale",Vector2(1,1),0.1)
 	tween.parallel().tween_property($CanvasGroup/easymode,"rotation_degrees",0,0.1)

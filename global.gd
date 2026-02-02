9ee71e888f4fc2a92d6184d04acab992f0bdb5d8
MahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2026-02-02 04:47:47 +0200

    moments before disaster

--- global.gd
+++ global.gd
@@ -4,3 +4,13 @@ extends Node2D
 @export var gamestate : GameState
 
 @export var colors : Dictionary[String,Color]
+
+var easymode := false
+
+
+func _input(event: InputEvent) -> void:
+	if(event.is_action_pressed("fullscreen")):
+		if(DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED):
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
--- losescreen.gd
+++ losescreen.gd
@@ -4,11 +4,18 @@ signal restart
 
 var score = 0
 var isover = false
+var waseasy = false
 
 func enter():
 	modulate.a = 0
 	score = 0
 	$CanvasGroup/score.text = "Score: " + str(score)
+	waseasy = Global.easymode
+	if(waseasy):
+		$CanvasGroup/score.modulate = Global.colors["blue"]
+	else:
+		$CanvasGroup/score.modulate = Color.WHITE
+	
 	var tween = create_tween()
 	tween.tween_property(self,"modulate:a",1.,0.3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
 	tween.tween_callback($Timer.start)
@@ -30,4 +37,29 @@ func _on_button_pressed(id) -> void:
 	var tween = create_tween()
 	tween.tween_property(self,"modulate:a",0,0.1).set_trans(Tween.TRANS_CIRC)
 	isover = false
-		
+
+func _on_text_button_pressed(id: int) -> void:
+	if(not isover):
+		return
+	$click.play()
+	if(Global.easymode):
+		disable_easy()
+	else:
+		enable_easy()
+
+func enable_easy():
+	Global.easymode = true
+	$CanvasGroup/retry.modulate = Global.colors["blue"]
+	$CanvasGroup/easymode.modulate = Global.colors["blue"]
+	var tween = get_tree().create_tween()
+	tween.tween_property($CanvasGroup/easymode,"scale",Vector2(1.1,1.1),0.1)
+	tween.parallel().tween_property($CanvasGroup/easymode,"rotation_degrees",10,0.1)
+
+
+func disable_easy():
+	Global.easymode = false
+	$CanvasGroup/retry.modulate = Color.WHITE
+	$CanvasGroup/easymode.modulate = Color.WHITE
+	var tween = get_tree().create_tween()
+	tween.tween_property($CanvasGroup/easymode,"scale",Vector2(1,1),0.1)
+	tween.parallel().tween_property($CanvasGroup/easymode,"rotation_degrees",0,0.1)
